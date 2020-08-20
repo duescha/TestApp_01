5ab0f95be482d87a2ef926a73632134bee05a3bc
Andreas Stepanow <andreas.stepanow@yahoo.de>
2020-08-20 10:59:16 +0000

    Init Commit

---
+++ TestApp_01/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("de.arvato.TestApp_01.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
